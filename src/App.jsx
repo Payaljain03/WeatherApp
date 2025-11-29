4e657842a682e597fb4f1646bca8ab4fc678b92c
Payal Jain <payaljain1503@gmail.com>
2025-11-29 19:35:58 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,6 @@
 import React from 'react'
-// import Count from './Count.jsx'
-// import PasswordInput from './PasswordInput.jsx'
 import WeatherApp from './WeatherApp.jsx' 
-
 export default function App(){
-  // return (
-  //   <>
-  //     <h5>Hello World</h5>
-  //       <Count/>
-  //         <PasswordInput/>
-  //       </> 
-
-  // )
   return(
     <>
       <WeatherApp/>
